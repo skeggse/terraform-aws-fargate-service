b14fc8095c4d35b964b991117f70749ed90bf6a4
Ryan Tasson <ryant@mixmax.com>
2020-02-13 12:22:22 -0500

    fix: use service name to grab task definition by default

--- locals.tf
+++ locals.tf
@@ -2,10 +2,6 @@ module "global_constants" {
   source = "git::ssh://git@github.com/mixmaxhq/terraform-global-constants.git?ref=v1.2.1"
 }
 
-data "aws_ecs_task_definition" "fargate_bootstrap" {
-  task_definition = "fargate-bootstrap-${var.environment}"
-}
-
 locals {
   aws_account_id = module.global_constants.aws_account_id[var.environment]
   aws_region     = module.global_constants.aws_region[var.environment]
@@ -19,7 +15,7 @@ locals {
   # ECS
   ecs_cluster      = "arn:aws:ecs:${local.aws_region}:${local.aws_account_id}:cluster/${var.environment}"
   ecs_cluster_name = var.environment
-  task_definition  = var.task_definition == "" ? "${data.aws_ecs_task_definition.fargate_bootstrap.family}:${data.aws_ecs_task_definition.fargate_bootstrap.revision}" : var.task_definition
+  task_definition  = var.task_definition == "" ? local.env_name : var.task_definition
 
   # Tagging
   default_tags = {
