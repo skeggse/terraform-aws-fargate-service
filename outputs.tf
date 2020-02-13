9e2af1ff03128fd8dce53e772aba02ba81917e38
Ryan Tasson <ryant@mixmax.com>
2020-02-13 16:40:39 -0500

    feat: expose IAM role name as output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "task_role_arn" {
   value       = aws_iam_role.task.arn
 }
 
+output "task_role_name" {
+  description = "The name of the IAM Role created for the Fargate service"
+  value       = aws_iam_role.task.name
+}
+
 output "task_sg_id" {
   description = "The ID of the Security Group attached to Fargate Tasks"
   value       = aws_security_group.task.id
